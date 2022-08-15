966f09bea5f3a69492fa9080284fe88c0e64c076
Nathan Colin <nathancolin27@gmail.com>
2022-08-15 15:47:59 +0200

    updated requirements

--- matplot.py
+++ matplot.py
@@ -1,5 +1,26 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy import stats
+
 
 df = pd.read_csv("data.csv")
-print(df.head())
+kms = list(df["km"])
+prices = list(df["price"])
+
+# plt.plot(kms, prices, 'o')
+plt.scatter(kms, prices)
+
+plt.title("Price of a vehicule depending on its mileage")
+plt.xlabel("kms")
+plt.ylabel("price")
+
+slope, intercept, r, p, std_err = stats.linregress(kms, prices)
+
+def myfunc(x):
+      return slope * x + intercept
+
+mymodel = list(map(myfunc, kms))
+plt.plot(kms, mymodel)
+
+plt.show()
